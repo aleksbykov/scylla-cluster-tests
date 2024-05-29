b0e4855af11ba404bbb607bcaf40c579a027df54
Aleksandr Bykov <alex.bykov@scylladb.com>
2024-05-29 21:31:01 +0700

    clean group0 also if non-voter found

--- sdcm/utils/raft/common.py
+++ sdcm/utils/raft/common.py
@@ -138,8 +138,8 @@ class NodeBootstrapAbortManager:
         self.bootstrap_node.stop_scylla_server(ignore_status=True, timeout=600)
         # Clean garbage from group 0 and scylla data and restart setup
         try:
-            if self.verification_node.raft.get_diff_group0_token_ring_members() and \
-                    not self.verification_node.raft.get_group0_non_voters():
+            if self.verification_node.raft.get_diff_group0_token_ring_members() or \
+                    self.verification_node.raft.get_group0_non_voters():
                 self.verification_node.raft.clean_group0_garbage(raise_exception=True)
             LOGGER.debug("Clean old scylla data and restart scylla service")
             self.bootstrap_node.clean_scylla_data()
