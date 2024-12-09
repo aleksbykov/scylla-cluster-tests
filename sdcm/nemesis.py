a496a96208563bfce11de979b4ead3561724f74e
Aleksandr Bykov <alex.bykov@scylladb.com>
2024-12-09 21:36:17 +0700

    fix(nemesis): set busy with nemesis asap

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -4855,14 +4855,14 @@ class Nemesis:  # pylint: disable=too-many-instance-attributes,too-many-public-m
 
             with temporary_replication_strategy_setter(node) as replication_strategy_setter:
                 new_node = self._add_new_node_in_new_dc()
+                # Mark a new node as "running nemesis" as soon as possible to prevent it be marked as "target node" by parallel nemesis.
+                # This new node should not be unset as running nemesis because the node will be terminated in the end of nemesis
+                # and removed from the list of nodes
+                self.set_current_running_nemesis(new_node)
                 node_added = True
                 status = self.tester.db_cluster.get_nodes_status()
                 new_dc_list = [dc for dc in list(status.keys()) if dc.endswith("_nemesis_dc")]
                 assert new_dc_list, "new datacenter was not registered"
-                # Mark a new node as "running nemesis" to prevent it be marked as "target node" by parallel nemesis.
-                # This new node should not be unset as running nemesis because the node will be terminated in the end of nemesis
-                # and removed from the list of nodes
-                self.set_current_running_nemesis(new_node)
                 new_dc_name = new_dc_list[0]
                 for keyspace in system_keyspaces + ["keyspace_new_dc"]:
                     strategy = ReplicationStrategy.get(node, keyspace)
