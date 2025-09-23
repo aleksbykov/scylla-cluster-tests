e892de59d77709eeb67758486f521a30c95d0729
Evgeniy Naydanov <evgeniy.naydanov@scylladb.com>
2025-09-23 17:49:24 +0300

    feature(test): LWT/MV/CDC longevity test with tablet split/merge
    New jenkins jobs for testing LWT/MV/CDC with tablets split/merge.

--- sdcm/logcollector.py
+++ sdcm/logcollector.py
@@ -1099,6 +1099,7 @@ class LoaderLogCollector(LogCollector):
         ),
         CommandLog(name="cloud-init-output.log", command="cat /var/log/cloud-init-output.log"),
         CommandLog(name="cloud-init.log", command="cat /var/log/cloud-init.log"),
+        FileLog(name="cdc-replicator.log", search_locally=True),
         FileLog(name="console_output.log", search_locally=True),
     ]
 
